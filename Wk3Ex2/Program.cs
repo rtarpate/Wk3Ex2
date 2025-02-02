277aa5fb7778127d71389b96d005c92d4601ab7f
Ashton Pate <arpate@amarillocollege.com>
2025-02-02 01:10:52 -0800

    puts code inside a loop

--- Wk3Ex2/Program.cs
+++ Wk3Ex2/Program.cs
@@ -1,41 +1,47 @@
-﻿
-//declares variable user input for yes
-double userinput;
-
-//declares varibale dice/random number
-Random Dice = new Random();
-
-
-//promts user input to roll dice
-Console.WriteLine(" do you want to roll a dice ");
-Console.WriteLine(" For yes input 1, For no input 2, ");
-
-//collects user input
-userinput = Convert.ToDouble(Console.ReadLine());
-
-//calutes the dice rolling
-if (userinput == 1)
+﻿while (true)
 {
-    //caluates random number of a D6
-    Console.WriteLine(" You rolled : " + Dice.Next(7));
+    //declares variable user input for yes
+    double userinput;
 
-    //promts user to play again
-    Console.WriteLine(" do you want to play again? ");
+    //declares varibale dice/random number
+    Random Dice = new Random();
+
+
+    //promts user input to roll dice
+    Console.WriteLine(" do you want to roll a dice ");
     Console.WriteLine(" For yes input 1, For no input 2, ");
 
     //collects user input
     userinput = Convert.ToDouble(Console.ReadLine());
 
+    //calutes the dice rolling
+    if (userinput == 1)
+    {
+        //caluates random number of a D6
+        Console.WriteLine(" You rolled : " + Dice.Next(7));
+
+        //promts user to play again
+        Console.WriteLine(" do you want to play again? ");
+        Console.WriteLine(" For yes input 1, For no input 2, ");
+
+        //collects user input
+        userinput = Convert.ToDouble(Console.ReadLine());
+
+        if (userinput == 2)
+        {
+            break;
+        }
 
 
-}
-//promts user to exit the program
-else if (userinput == 2)
-{
-    //outputs final lines
-    Console.WriteLine("Thank you for playing");
-    Console.ReadLine();
+    }
+    //promts user to exit the program
+    else if (userinput == 2)
+    {
+        //outputs final lines
+        Console.WriteLine("Thank you for playing");
+        Console.ReadLine();
 
 
+    }
 }
 
